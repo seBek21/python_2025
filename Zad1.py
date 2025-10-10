df4d5eb424b49c8a81de21dddf9977d2272605c1
seBek21 <77143840+seBek21@users.noreply.github.com>
2025-10-10 09:47:01 +0200

    Add files via upload

---
+++ Zad1.py
@@ -0,0 +1,20 @@
+'''Zadanie 1. wyznacz wartosc wyrazenia 10^2 + 136:4.15 * sqr(2)
+Wynik zapisz do zmiennej x'''
+
+
+'''Zadanie 2. Oblicz wskaznik BMI osoby, ktorej waga = 139kg
+a wzrost = 211cm. Wynik zapisz do zmiennej BMI'''
+
+
+'''Zadanie 3. Zdefiniuj sekwencje seq="AGGTCTCAGGCGCTATCA" 
+nastepnie zamien wszystkie tyminy na uracyl. Ile uracyli znajduje sie w sekwencji?'''
+
+
+'''Zadanie 4. Sprawdz czy wyrazenie x^14 +5x -145 jest wieksze
+od wyrazenia x^12 +10x -221 dla x=3'''
+
+
+'''Zadanie 5. Z sekwencji seq = "AGGTCTCAGGCGCTATCA" utworz liste
+gdzie kazdy element to pojedynczy nukleotyd. Nastepnie stworz liste
+uniklanych nukleotydow oraz slownik w ktorym kluczem beda uniklane nukleotydy
+a wartoscia liczba ich wystapien w sekwencji.'''
