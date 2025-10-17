eb5eb4097758bfb43c9ec0643d8a589eb8446807
seBek21 <77143840+seBek21@users.noreply.github.com>
2025-10-17 11:31:07 +0200

    Add files via upload

---
+++ cw2_zad.py
@@ -0,0 +1,40 @@
+'''
+Zadanie 1.
+Korzystając z instrukcji if/else wyznacz liczbę miejsc zerowych równania kwadratowego ax^2 + bx + c = 0.
+Pamiętaj, że liczba miejsc zerowych zależy od wartości parametru Delta = b^2 − 4ac.
+
+Jeżeli Delta > 0 — równanie ma dwa różne miejsca zerowe.
+Jeżeli Delta = 0 — równanie ma jedno (podwójne) miejsce zerowe.
+Jeżeli Delta < 0 — równanie nie ma miejsc zerowych w zbiorze liczb rzeczywistych.
+
+W zadaniu zaimplementuj sprawdzenie wartości Delty przy użyciu if/else i wypisz odpowiedni komunikat.
+'''
+
+
+
+'''
+Zadanie 2.
+Korzystając z petli while zbuduj listę składającą się z x elementów, 
+gdzie każdy następny element to suma dwóch poprzednich z uwzględnieniem, 
+że pierwsze dwa elementy listy to [0,1]. 
+Innymi słowy zakoduj pętle, która buduje ciąg Fibonacciego o zadanej długości.
+'''
+
+
+'''
+Zadanie 3. 
+Korzystajac z petli for i wyrazen if/else podziel liste a = [1,7,10,11,23,156,211,458,19,6]
+na dwie listy jedna zawierajaca liczby parzyste druga zawierajaca liczby nieparzyste
+'''
+
+
+'''
+Zadanie 4.
+Korzystajac z listy skladanej utworz liste pierwiastkow n kolejnych liczb naturalnych
+'''
+
+'''
+Zadanie 5.
+Zdefiniuj funkcje prime_number ktora jako argument przyjmuje liczbe x i zwraca
+czy dana przez uzytkownika liczba jest liczba pierwsza
+'''
